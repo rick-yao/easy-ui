bece7b39ce9dc1e7808fbdf6280218fe106a8276
rick <abneryaoo@gmail.com>
2022-05-23 13:02:58 +0800

    basic doc page and css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,24 @@
 <template>
   <Topnav/>
-  <div>doc</div>
+  <div class="content">
+    <aside>
+      <h2>组件列表</h2>
+      <ol>
+        <li>
+          <router-link to="/doc/switch">Switch组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/button">Button组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/dialog">Dialog组件</router-link>
+        </li>
+        <li>
+          <router-link to="/doc/tabs">Tabs组件</router-link>
+        </li>
+      </ol>
+    </aside>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -9,9 +27,27 @@ import Topnav from './Topnav.vue';
 </script>
 <script lang="ts">
 export default {
-  name:"Doc"
-}
+  name: 'Doc'
+};
 </script>
-<style scoped>
+<style scoped lang="scss">
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  padding-top: 70px;
 
+  > h2 {
+    margin-bottom: 4px;
+  }
+
+  > ol {
+    > li {
+      padding: 4px 0;
+    }
+  }
+}
 </style>
