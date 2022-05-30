7e096e152d4b750dd24dcad382a9c9517ce6c62b
rick <abneryaoo@gmail.com>
2022-05-30 14:36:14 +0800

    support visible

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,13 +1,26 @@
 <template>
-  <Dialog/>
+  <div>dialog示例</div>
+  <h1>示例1</h1>
+  <Button @click="toggle" >toggle</Button>
+  <Dialog :visible="x"/>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue';
+import Button from '../lib/Button.vue';
+import {ref} from 'vue';
 export default {
   name: "DialogDemo",
   components:{
+    Button,
     Dialog
+  },
+  setup(){
+    const x = ref(false)
+    const toggle = ()=>{
+      x.value = !x.value
+    }
+    return {x,toggle}
   }
 
 }
