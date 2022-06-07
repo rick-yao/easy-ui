957e6799edfe56f3cbc6c84cc1db453dcb311806
rick <abneryaoo@gmail.com>
2022-06-07 15:56:42 +0800

    switch content through click

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="gulu-tabs">
     <div class="gulu-tabs-nav">
-      <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="gulu-tabs-nav-item" :class="{selected: t===selected}" @click="select(t)" v-for="(t,index) in titles"
+           :key="index">{{ t }}
+      </div>
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+      <component class="gulu-tabs-content-item"  :is="current" :key="current.props.title"/>
     </div>
   </div>
 </template>
@@ -12,8 +14,14 @@
 <script lang="ts">
 
 import Tab from './Tab.vue';
+import {computed} from 'vue';
 
 export default {
+  props: {
+    selected: {
+      type: String
+    }
+  },
   name: 'Tabs',
   setup(props: any, context: any) {
     const defaults = context.slots.default();
@@ -22,10 +30,18 @@ export default {
         throw new Error('Tabs里的组件必须是Tab');
       }
     });
-    const titles = defaults.map((tag: { props: { title: string; }; })=>{
-      return tag.props.title
+    const select = (t: string) => {
+      context.emit('update:selected', t);
+    };
+    const titles = defaults.map((tag: { props: { title: string; }; }) => {
+      return tag.props.title;
+    });
+    const current = computed(()=>{
+     return defaults.filter((tag: { props: { title: string; }; }) => {
+        return tag.props.title === props.selected;
+      })[0];
     })
-    return {defaults,titles};
+    return {defaults, titles, current, select};
   }
 
 };
@@ -40,18 +56,22 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
   }
+
   &-content {
     padding: 8px 0;
   }
