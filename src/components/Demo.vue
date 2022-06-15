1ebc4a5fbf5caf8acd48a0aea591562cca41ff47
rick <abneryaoo@gmail.com>
2022-06-15 20:46:51 +0800

    demo component

---
+++ src/components/Demo.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="demo">
+    <h2>{{title}}</h2>
+    <div class="demo-component">
+      <component :is="component" />
+    </div>
+    <div class="demo-actions">
+      <Button @click="codeVisible =! codeVisible">查看代码</Button>
+    </div>
+    <div class="demo-code" v-if="codeVisible">
+      <pre ><code>{{code}}</code></pre>
+    </div>
+
+  </div>
+</template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+import {Base64} from 'js-base64';
+import {ref} from 'vue';
+
+export default {
+  components:{
+    Button
+  },
+  props: {
+    component: Object,
+    title:{
+      type:String
+    },
+    base64:{
+      type:String
+    }
+  },
+  setup(props:any,context:any) {
+    const code = Base64.decode(props.base64)
+    console.log(code);
+    const codeVisible = ref(false)
+    return {
+      code,
+      codeVisible
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+  &-component {
+    padding: 16px;
+  }
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+    >pre {
+      word-wrap:break-word;
+      white-space: break-spaces;
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,83 +1,26 @@
 <template>
   <div>
     <h1>Switch 组件示例 </h1>
-    <div class="demo">
-      <h2>常规用法</h2>
-      <div class="demo-component">
-        <Switch1Demo/>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre><code>{{code1}}</code></pre>
-      </div>
-    </div>
-    <div class="demo">
-      <h2>支持 disabled </h2>
-      <div class="demo-component">
-        <Switch2Demo/>
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre><code >{{code2}}</code></pre>
-      </div>
-    </div>
+   <Demo :component="Switch1Demo" title="常规用法" base64="PHRlbXBsYXRlPgogIDxTd2l0Y2ggdi1tb2RlbDp2YWx1ZT0iYm9vbCIgLz4KPC90ZW1wbGF0ZT4KCjxzY3JpcHQgbGFuZz0idHMiPgppbXBvcnQgU3dpdGNoIGZyb20gJy4uL2xpYi9Td2l0Y2gudnVlJwppbXBvcnQgewogIHJlZgp9IGZyb20gJ3Z1ZScKZXhwb3J0IGRlZmF1bHQgewogIGNvbXBvbmVudHM6IHsKICAgIFN3aXRjaCwKICB9LAogIHNldHVwKCkgewogICAgY29uc3QgYm9vbCA9IHJlZihmYWxzZSkKICAgIHJldHVybiB7CiAgICAgIGJvb2wKICAgIH0KICB9Cn0KPC9zY3JpcHQ+"/>
+    <Demo :component="Switch2Demo" title="支持disabled" base64="PHRlbXBsYXRlPgogIDxTd2l0Y2ggdi1tb2RlbDp2YWx1ZT0iYm9vbCIgZGlzYWJsZWQgLz4KPC90ZW1wbGF0ZT4KCjxzY3JpcHQgbGFuZz0idHMiPgppbXBvcnQgU3dpdGNoIGZyb20gJy4uL2xpYi9Td2l0Y2gudnVlJwppbXBvcnQgewogIHJlZgp9IGZyb20gJ3Z1ZScKZXhwb3J0IGRlZmF1bHQgewogIGNvbXBvbmVudHM6IHsKICAgIFN3aXRjaCwKICB9LAogIHNldHVwKCkgewogICAgY29uc3QgYm9vbCA9IHJlZihmYWxzZSkKICAgIHJldHVybiB7CiAgICAgIGJvb2wKICAgIH0KICB9Cn0KPC9zY3JpcHQ+"/>
   </div>
 </template>
 
 <script>
 import Switch from '../lib/Switch.vue'
-import Button from '../lib/Button.vue'
 import {ref} from "vue";
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-import Prism from 'prismjs'
-import 'prismjs/themes/prism.css'
-import { Base64 } from 'js-base64';
-let code1 = Base64.decode('PHRlbXBsYXRlPgogIDxTd2l0Y2ggdi1tb2RlbDp2YWx1ZT0iYm9vbCIgLz4KPC90ZW1wbGF0ZT4KCjxzY3JpcHQgbGFuZz0idHMiPgppbXBvcnQgU3dpdGNoIGZyb20gJy4uL2xpYi9Td2l0Y2gudnVlJwppbXBvcnQgewogIHJlZgp9IGZyb20gJ3Z1ZScKZXhwb3J0IGRlZmF1bHQgewogIGNvbXBvbmVudHM6IHsKICAgIFN3aXRjaCwKICB9LAogIHNldHVwKCkgewogICAgY29uc3QgYm9vbCA9IHJlZihmYWxzZSkKICAgIHJldHVybiB7CiAgICAgIGJvb2wKICAgIH0KICB9Cn0KPC9zY3JpcHQ+')
-let code2 =Base64.decode('PHRlbXBsYXRlPgogIDxTd2l0Y2ggdi1tb2RlbDp2YWx1ZT0iYm9vbCIgZGlzYWJsZWQgLz4KPC90ZW1wbGF0ZT4KCjxzY3JpcHQgbGFuZz0idHMiPgppbXBvcnQgU3dpdGNoIGZyb20gJy4uL2xpYi9Td2l0Y2gudnVlJwppbXBvcnQgewogIHJlZgp9IGZyb20gJ3Z1ZScKZXhwb3J0IGRlZmF1bHQgewogIGNvbXBvbmVudHM6IHsKICAgIFN3aXRjaCwKICB9LAogIHNldHVwKCkgewogICAgY29uc3QgYm9vbCA9IHJlZihmYWxzZSkKICAgIHJldHVybiB7CiAgICAgIGJvb2wKICAgIH0KICB9Cn0KPC9zY3JpcHQ+')
+import Demo from "./Demo.vue";
 export default {
   name: "SwitchDemo",
-  components: {Switch, Button,Switch1Demo,Switch2Demo},
+  components: {Demo, Switch, Switch1Demo,Switch2Demo},
   setup() {
     const bool = ref(true)
-    return {bool , Prism,code1,code2}
+    return {bool ,Switch1Demo, Switch2Demo}
   }
 }
 </script>
 
 <style scoped lang="scss">
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-
-  > h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-
-  &-component {
-    padding: 16px;
-  }
-
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
-    }
-  }
-}
 </style>
