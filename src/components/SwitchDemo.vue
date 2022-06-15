872a56250c793b4f44370b6d196de04bebd35c8c
rick <abneryaoo@gmail.com>
2022-06-15 20:17:23 +0800

    show code

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>&lt;Switch v-model:value="bool" /&gt;</pre>
+        <pre><code>{{code1}}</code></pre>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre class="language-html" v-html="Prism.highlight(code,Prism.languages.html,'html')"></pre>
+        <pre><code >{{code2}}</code></pre>
       </div>
     </div>
   </div>
@@ -36,13 +36,15 @@ import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
 import Prism from 'prismjs'
 import 'prismjs/themes/prism.css'
-const code = ` &lt;Switch v-model:value="bool" /&gt;`
+import { Base64 } from 'js-base64';
+let code1 = Base64.decode('PHRlbXBsYXRlPgogIDxTd2l0Y2ggdi1tb2RlbDp2YWx1ZT0iYm9vbCIgLz4KPC90ZW1wbGF0ZT4KCjxzY3JpcHQgbGFuZz0idHMiPgppbXBvcnQgU3dpdGNoIGZyb20gJy4uL2xpYi9Td2l0Y2gudnVlJwppbXBvcnQgewogIHJlZgp9IGZyb20gJ3Z1ZScKZXhwb3J0IGRlZmF1bHQgewogIGNvbXBvbmVudHM6IHsKICAgIFN3aXRjaCwKICB9LAogIHNldHVwKCkgewogICAgY29uc3QgYm9vbCA9IHJlZihmYWxzZSkKICAgIHJldHVybiB7CiAgICAgIGJvb2wKICAgIH0KICB9Cn0KPC9zY3JpcHQ+')
+let code2 =Base64.decode('PHRlbXBsYXRlPgogIDxTd2l0Y2ggdi1tb2RlbDp2YWx1ZT0iYm9vbCIgZGlzYWJsZWQgLz4KPC90ZW1wbGF0ZT4KCjxzY3JpcHQgbGFuZz0idHMiPgppbXBvcnQgU3dpdGNoIGZyb20gJy4uL2xpYi9Td2l0Y2gudnVlJwppbXBvcnQgewogIHJlZgp9IGZyb20gJ3Z1ZScKZXhwb3J0IGRlZmF1bHQgewogIGNvbXBvbmVudHM6IHsKICAgIFN3aXRjaCwKICB9LAogIHNldHVwKCkgewogICAgY29uc3QgYm9vbCA9IHJlZihmYWxzZSkKICAgIHJldHVybiB7CiAgICAgIGJvb2wKICAgIH0KICB9Cn0KPC9zY3JpcHQ+')
 export default {
   name: "SwitchDemo",
   components: {Switch, Button,Switch1Demo,Switch2Demo},
   setup() {
     const bool = ref(true)
-    return {bool , Prism,code}
+    return {bool , Prism,code1,code2}
   }
 }
 </script>
@@ -71,7 +73,6 @@ $border-color: #d9d9d9;
   &-code {
     padding: 8px 16px;
     border-top: 1px dashed $border-color;
-
     > pre {
       line-height: 1.1;
       font-family: Consolas, 'Courier New', Courier, monospace;
