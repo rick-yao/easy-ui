3ecc8aae360946265900d0ad2f1801f30c592ee1
rick <abneryaoo@gmail.com>
2022-06-28 15:53:35 +0800

    update readme and intro

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -3,7 +3,7 @@
     <h1>Easy UI</h1>
     <p>Easy UI是一款基于Vue3 和 Typescript 的UI组件库</p>
     <p>这款组件库是我从Vue2过渡到Vue3的练习作品，为了熟悉Vue3，这款UI库全程手写，尽量做到熟悉Vue3写法和相比于Vue2的变化</p>
-    <p>所以强烈不建议将此库用于生产环境，但可以相互学习交流，源代码在GitHub.com</p>
+    <p>所以强烈不建议将此库用于生产环境，但可以相互学习交流，源代码在 <a href="https://github.com/rick-yao/easy-ui">easy-ui</a></p>
   </article>
 </template>
 
